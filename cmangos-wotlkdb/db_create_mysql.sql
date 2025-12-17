a4d3807d5e4fb120cc8641cf6abfb973d86ec04b
Filip Dadgar <filip.dadgar.ext@t-hive.io>
2025-12-17 14:33:58 +0100

    fix

--- cmangos-wotlkdb/db_create_mysql.sql
+++ cmangos-wotlkdb/db_create_mysql.sql
@@ -8,9 +8,17 @@ CREATE DATABASE IF NOT EXISTS logs DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 CREATE USER IF NOT EXISTS 'mangos'@'%' IDENTIFIED BY 'mangos';
 ALTER USER 'mangos'@'%' IDENTIFIED BY 'mangos';
 
+CREATE USER IF NOT EXISTS 'mangos'@'localhost' IDENTIFIED BY 'mangos';
+ALTER USER 'mangos'@'localhost' IDENTIFIED BY 'mangos';
+
 GRANT ALL PRIVILEGES ON wotlkrealmd.* TO 'mangos'@'%';
 GRANT ALL PRIVILEGES ON wotlkcharacters.* TO 'mangos'@'%';
 GRANT ALL PRIVILEGES ON wotlkmangos.* TO 'mangos'@'%';
 GRANT ALL PRIVILEGES ON logs.* TO 'mangos'@'%';
+
+GRANT ALL PRIVILEGES ON wotlkrealmd.* TO 'mangos'@'localhost';
+GRANT ALL PRIVILEGES ON wotlkcharacters.* TO 'mangos'@'localhost';
+GRANT ALL PRIVILEGES ON wotlkmangos.* TO 'mangos'@'localhost';
+GRANT ALL PRIVILEGES ON logs.* TO 'mangos'@'localhost';
 
 FLUSH PRIVILEGES;
